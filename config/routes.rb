72c70d892f5848761b45a2f4cdb339d3eee45b63
Preston Lam <preslam911@gmail.com>
2025-12-07 15:44:23 -0700

    fixed the extra space in code block

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,4 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-
 end
